f5cf9084643968731c48659b3843f470f94a55be
poorvashelke <poorvashelke@gmail.com>
2019-05-24 16:35:21 -0700

    hashing excersize

--- my_hash_map/lib/p02_hashing.rb
+++ my_hash_map/lib/p02_hashing.rb
@@ -4,11 +4,23 @@ end
 
 class Array
   def hash
+    return 2372439028402 if self.empty?
+    sum = self.first
+    self.each do |i|
+      sum += i
+    end
+    sum
   end
 end
 
 class String
   def hash
+    alphabet = ("a".."z").to_a 
+    sum = (alphabet.index(self.downcase[-1]) + 2).hash
+    self.each_char do |char|
+      sum += alphabet.index(char.downcase)
+    end
+    sum
   end
 end
 
@@ -16,6 +28,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    sum = 0
+    self.values do |value|
+      sum += value.hash
+    end
+    sum
   end
 end
