d19dea8c669c30dc1b47a7fd17452b80ac0a3593
poorvashelke <poorvashelke@gmail.com>
2019-05-24 17:05:35 -0700

    hashing

--- my_hash_map/lib/p03_hash_set.rb
+++ my_hash_map/lib/p03_hash_set.rb
@@ -1,23 +1,49 @@
-class HashSet
-  attr_reader :count
+require "byebug"
 
-  def initialize(num_buckets = 8)
+class HashSet
+  attr_reader :store
+  attr_accessor :count
+
+  def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
 
-  def insert(key)
+  def insert(num)
+    x = num.hash
+    unless include?(num)
+      self[x] << x
+      self.count += 1
+      self.resize?
+    end
+  end
+  
+  def remove(num)
+    #debugger
+    x = num.hash
+    if include?(num)
+      @count -= 1
+      self[x].delete(x)
+    end
+    
   end
 
-  def include?(key)
+  def include?(num)
+    x = num.hash
+    self[x].each do |check|
+      return true if check == x
+    end
+    false
   end
 
-  def remove(key)
+  def resize?
+    resize! if num_buckets == count
   end
 
-  private
+  #private
 
   def [](num)
+    store[num % num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
@@ -26,5 +52,22 @@ class HashSet
   end
 
   def resize!
+    new_store = store.flatten
+    self.count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+    new_store.each do |single|
+      insert(single)
+    end 
   end
 end
+
+h = HashSet.new
+p h
+puts
+h.insert(1)
+p h
+puts
+p h[1]
+
+puts 
+p h.remove(1)
